c4e01144900c078191214feef9b6b82685105830
SinaRiahi <sinariahi1382@gmail.com>
2026-08-14 13:16:09 +0330

    ...

--- Projects/project_02/backend/main.py
+++ Projects/project_02/backend/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+class ProjectCreate(BaseModel):
+    name: str = Field(min_length=3,max_length=100)
+    description: str = Field(max_length=1000)
+    priority: int = Field(ge=1,le=5)
+
+class ProjectResponse(BaseModel):
+    name: str
+    description: str
+    priority: int
+
 app.mount("/statics", StaticFiles(directory="statics"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -20,9 +32,15 @@ def health_check():
         }
 
 
-@app.get("/api/projects")
+@app.get("/api/projects", response_model=ProjectResponse)
 def create_project():
     return [
         {"id":1, "name": "FastAPI extra"}
     ]
 
+@app.get("/api/projects/{id}", response_model=ProjectResponse)
+def get_project(id:int):
+    return {
+        "id":id,
+        "name":" "
+    }
